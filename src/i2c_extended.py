84a2de59f7a261cc8def8b860faefcc48af5cfbb
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-09-17 23:22:23 +0300

    add a module to keep the old I2C r/w methods (to much with the new one)

--- src/i2c_extended.py
+++ src/i2c_extended.py
@@ -1,28 +1,27 @@
-import microbit
+# I2C functions
+def __i2c_write_reg_8(addr, reg, val):
+    microbit.i2c.write(addr, bytes([reg, val]), False)
 
-class I2C:
-    def __init__(self, address):
-        """
-        address - address of the device to communicate with
-        """
-        self.__write = microbit.i2c.write
-        self.__read = microbit.i2c.read
-        self.address = address
+def __i2c_write_reg_16(addr, reg, val):
+    microbit.i2c.write(addr, bytes([reg, ((val >> 8) & 0xFF), (val & 0xFF)]), False)
 
-    def read(self, reg, no_bytes, repeat = False):
-        """
-        reg - the register to read from in bytes form object
-        no_bytes - how many bits to read; a multiplier of 8
-        repeat - specifies if a stop bit is sent
-        returns - data in bytes format
-        """
-        self.__write(self.address, reg, repeat)
-        return self.__read(self.address, no_bytes, repeat)
+def __i2c_write_reg_32(addr, reg, val):
+    microbit.i2c.write(addr, bytes([reg, ((val >> 24) & 0xFF), ((val >> 16) & 0xFF), ((val >> 8) & 0xFF), (val & 0xFF)]), False)
 
-    def write(self, reg, args, repeat = False):
-        """
-        reg - register to write to; bytes object
-        args - values to write to register; bytes object
-        repeat - specifies if a stop bit is sent
-        """
-        self.__write(self.address, reg + args, repeat)
+def __i2c_write_reg_list(addr, reg, list):
+    arr = [reg]
+    arr.extend(list)
+    microbit.i2c.write(addr, bytes(arr), False)
+
+def __i2c_read_reg_8(addr, reg):
+    microbit.i2c.write(addr, bytes([reg]), False)
+    return microbit.i2c.read(addr, 1, False)[0]
+
+def __i2c_read_reg_16(addr, reg):
+    microbit.i2c.write(addr, bytes([reg]), False)
+    buf = microbit.i2c.read(addr, 2, False)
+    return ((buf[0] << 8) | buf[1])
+
+def __i2c_read_reg_list(addr, reg, len):
+    microbit.i2c.write(addr, bytes([reg]), False)
+    return microbit.i2c.read(addr, len, False)
