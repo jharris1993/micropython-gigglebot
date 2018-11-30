cbcb58df1a9985bbe801ec5535d696b6b7efc730
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-12-01 00:31:29 +0200

    add echoes for debugging purposes in integration script

--- integration/deploy.sh
+++ integration/deploy.sh
@@ -10,16 +10,19 @@ payload=$(
      --arg body "$(cat $TRAVIS_BUILD_DIR/integration/changelog.md)" \
      '{ tag_name: $tag, name: $name, body: $body, draft: false }'
 )
+echo "payload is: $payload"
 
 # release to github
 upload_url=$(curl -X POST \
     https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases?access_token=$GITHUB_TOKEN \
     --header "Content-Type: application/json" \
     -d "$payload" | jq -r .upload_url | sed -e "s/{?name,label}//")
+echo "upload url is: $upload_url"
 
 # release all assets
 for item in $(ls build/*.tar.gz build/*.hex)
 do
+    echo "uploading $item"
     curl "$upload_url?name=$item&access_token=$GITHUB_TOKEN" \
         --header "Content-Type: application/octet-stream" \
         --data-binary @"$item"
