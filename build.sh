d05389790c36b9eac8c5c56cabf5011455ab8c40
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-12-01 15:11:11 +0200

    use cache instead of pushing the image to the hub

--- build.sh
+++ build.sh
@@ -10,18 +10,18 @@ function sanitize() {
    echo "${s,,}"                # convert to lowercase
 }
 
-function docker_tag_exists() {
-    curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
-}
+# function docker_tag_exists() {
+#     curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
+# }
 
 SANITIZED_BRANCH=$(sanitize $TRAVIS_BRANCH)
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 IMAGE_NAME=$DOCKER_USERNAME/gupy-build-cache:$SANITIZED_BRANCH
 
-if docker_tag_exists $DOCKER_USERNAME/gupy-build-cache $SANITIZED_BRANCH; then
-    docker image pull $IMAGE_NAME
-fi
+# if docker_tag_exists $DOCKER_USERNAME/gupy-build-cache $SANITIZED_BRANCH; then
+#     docker image pull $IMAGE_NAME
+# fi
 
 docker image build -t $IMAGE_NAME src
 [[ "$(docker ps -a | grep gupy-container)" ]] && docker container rm -f gupy-container
@@ -32,8 +32,9 @@ mkdir build
 docker container cp gupy-container:/src/gupy/build/firmware.hex build/
 # copy to build/tmp all py/mpy files
 docker container cp gupy-container:/src/tmp/ build/
+
 # update cache image on docker hub
-docker image push $IMAGE_NAME
+# docker image push $IMAGE_NAME
 
 tar -cvzf build/$(cat $TRAVIS_BUILD_DIR/integration/tag)-mpy-modules.tar.gz build/tmp/*.mpy
 tar -cvzf build/$(cat $TRAVIS_BUILD_DIR/integration/tag)-py-modules.tar.gz build/tmp/*.py
